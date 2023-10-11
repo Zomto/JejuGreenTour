5b8380ed99d0cc052d4188240e927894392c7e0d
BigbabyKim <el2013@naver.com>
2023-10-11 15:28:49 +0900

    1011 1528

--- src/main/java/com/jejugreentour/jgt/accom/controller/AccomController.java
+++ src/main/java/com/jejugreentour/jgt/accom/controller/AccomController.java
@@ -1,10 +1,21 @@
 package com.jejugreentour.jgt.accom.controller;
 
+
 import com.jejugreentour.jgt.accom.service.AccomService;
+import com.jejugreentour.jgt.accom.vo.MainAccomImgVO;
+import com.jejugreentour.jgt.accom.vo.MainAccomVO;
+import com.jejugreentour.jgt.member.vo.MemberVO;
+import com.jejugreentour.jgt.util.UploadUtil;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/accom")
@@ -17,4 +28,46 @@ public class AccomController {
     public String addAccomForm(){
         return "content/accom/accom_add";
     }
+
+    // 숙박업소 등록
+    @PostMapping("/addAccom")
+    public String addAccom(MainAccomVO mainAccomVO, MultipartFile mainImg, MultipartFile[] subImg, HttpSession httpSession){
+
+        MemberVO loginInfo = (MemberVO)httpSession.getAttribute("loginInfo");
+        mainAccomVO.setAccomCeo(loginInfo.getMemberId());
+
+        // -- 상품 이미지 등록 --//
+
+        // 0. 다음에 들어 가야 할 ITEM_CODE 조회
+        String accomCode = accomService.selectNextAccomCode();
+
+
+//        // 1. 상품 이미지 정보가 다 들어갈 통
+//        List<MainAccomImgVO> mainAccomImgList = new ArrayList<>();
+
+
+        // 2. 이미지 정보 하나가 들어갈 수 있는 통!
+        // 첨부파일 기능
+        MainAccomImgVO vo = UploadUtil.uploadFile(mainImg);
+
+
+        // 첨부파일 기능 다중업로드
+        List<MainAccomImgVO> mainAccomImgList = UploadUtil.multiFileUpload(subImg);
+        mainAccomImgList.add(vo);
+
+        for(MainAccomImgVO mainAccomImgVO : mainAccomImgList) {
+            mainAccomImgVO.setAccomCode(accomCode);
+        }
+
+        mainAccomVO.setMainAccomImgList(mainAccomImgList);
+
+        // 상품 등록 + 이미지 등록 쿼리
+        mainAccomVO.setAccomCode(accomCode);
+        accomService.addAccom(mainAccomVO);
+
+        // 상품 이미지 정보 등록 쿼리
+
+        return "redirect:/admin/regItemForm";
+    }
+
 }
---
+++ src/main/java/com/jejugreentour/jgt/util/ConstantVariable.java
@@ -0,0 +1,10 @@
+package com.jejugreentour.jgt.util;
+
+// 상수 모음집
+public class ConstantVariable {
+
+    // 파일 업로드 경로
+    public static final String UPLOAD_PATH = "D:\\dev\\workspaceSpring\\Shop\\src\\main\\resources\\static\\images\\";
+
+
+}
