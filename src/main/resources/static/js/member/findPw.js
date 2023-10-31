469b3ed077ca9b04a71714cd6f76550c3c8693e2
zomto <lsj8490@naver.com>
2023-10-31 17:14:36 +0900

    1031 1714

--- src/main/resources/static/js/member/findPw.js
+++ src/main/resources/static/js/member/findPw.js
@@ -30,11 +30,7 @@ function startTimer(duration) {
     const timerInterval = setInterval(updateTimer, 1000);
     timerstop = timerInterval;
     // 타이머가 종료되면 interval 정지
-<<<<<<< HEAD
-    settimerstop= setTimeout(() => {
-=======
     settimerstop = setTimeout(() => {
->>>>>>> 83a6a44fec44cb4a50def74bcbc8a7d776db12c9
         clearInterval(timerInterval);
     }, duration);
 
@@ -42,82 +38,22 @@ function startTimer(duration) {
 
 function verifyCode() {
     var emailRegex = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
-<<<<<<< HEAD
-
-    if (emailRegex.test(document.querySelector('#memberEmail').value + document.querySelector('#email_host').value)) {
-        if(timerstop != null){
-            console.log('이전 타이머 있음');
-            clearTimeout(timerstop);
-            console.log(timerstop);
-        }
-        document.querySelector('#member_mail').value = (document.querySelector('#memberEmail').value + document.querySelector('#email_host').value)
-        clearTimeout(settimerstop);
-        clearTimeout(reatimerstop);
-        clearInterval(timerstop);
-        document.getElementById("timer").textContent = "03:00";
-        const additionalInputDiv = document.getElementById("additionalInput");
-        additionalInputDiv.style.display = "block";
-        startTimer(180000);
-        document.querySelector('#verify_code').value = null;
-        fetch('/member/verifyCode', { //요청경로
-=======
     var email = document.querySelector('#memberEmail').value + document.querySelector('#email_host').value;
 
     if (emailRegex.test(email)) {
         fetch('/member/checkInfo', {
->>>>>>> 83a6a44fec44cb4a50def74bcbc8a7d776db12c9
             method: 'POST',
             cache: 'no-cache',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
             },
-<<<<<<< HEAD
-            //컨트롤러로 전달할 데이터
-            body: new URLSearchParams({
-                email: document.querySelector('#memberEmail').value+document.querySelector('#email_host').value
-=======
             body: new URLSearchParams({
                 "memberId": document.querySelector("#memberId").value,
                 "member_mail": email
->>>>>>> 83a6a44fec44cb4a50def74bcbc8a7d776db12c9
             })
         })
             .then((response) => {
                 if (!response.ok) {
-<<<<<<< HEAD
-                    alert('fetch error!\n컨트롤러로 통신중에 오류가 발생했습니다.');
-                    return;
-                }
-
-                return response.text(); //컨트롤러에서 return하는 데이터가 없거나 int, String 일 때 사용
-                // return response.json(); //나머지 경우에 사용
-            })
-            //fetch 통신 후 실행 영역
-            .then((data) => {//data -> controller에서 리턴되는 데이터!
-                document.querySelector('.alertbox').innerHTML="인증메일이 발송 되었습니다! 메일함을 확인해주세요!"
-                document.querySelector('.alertbox').style.color='black';
-        
-                confirmCode = data;
-                reatimerstop= setTimeout(() => {
-                    confirmCode = null
-                }, 180000)
-
-            })
-    } else {
-        alert("이메일을 다시 확인 해주세요!")
-        const additionalInputDiv = document.getElementById("additionalInput");
-  
-            additionalInputDiv.style.display = "none";
-    }
-}
-function findId() {
-    const memberMail = document.getElementById("member_mail").value;
-    const mailTail = document.querySelector('#email_host').value;
-    console.log(memberMail);
-    console.log(mailTail);
-
-    fetch('/member/findId', { //요청경로
-=======
                     throw new Error('fetch error! 컨트롤러로 통신 중에 오류가 발생했습니다.');
                 }
                 return response.text();
@@ -127,9 +63,13 @@ function findId() {
                 let resultDiv = document.querySelector('.result');
 
                 if (data == "false") {
-                    resultDiv.innerHTML = "해당 아이디와 메일이 일치하지 않습니다. 더 이상 도와줄 방법이 없네용~ ";
+                    document.querySelector('.bi.bi-envelope-at-fill').setAttribute("fill", "#ff3f3f");
+                    resultDiv.innerHTML = "해당 아이디와 메일이 일치하지 않습니다.";
+                    resultDiv.style = "color : #ff3f3f"
+    
                     return;
                 } else{
+                    resultDiv.innerHTML = "";
                     if (timerstop !== null) {
                         console.log('이전 타이머 있음');
                         clearTimeout(timerstop);
@@ -193,46 +133,11 @@ function findId() {
     const mailTail = document.querySelector('#email_host').value;
 
     fetch('/member/findId', {
->>>>>>> 83a6a44fec44cb4a50def74bcbc8a7d776db12c9
         method: 'POST',
         cache: 'no-cache',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
         },
-<<<<<<< HEAD
-        //컨트롤러로 전달할 데이터
-        body: new URLSearchParams({
-           "member_mail1" : document.querySelector('#memberEmail').value+document.querySelector('#email_host').value
-        })
-    })
-    .then((response) => {
-        if(!response.ok){
-            alert('fetch error!\n컨트롤러로 통신중에 오류가 발생했습니다.');
-            return ;
-        }
-    
-        //return response.text(); //컨트롤러에서 return하는 데이터가 없거나 int, String 일 때 사용
-        return response.json(); //나머지 경우에 사용
-    })
-    //fetch 통신 후 실행 영역
-    .then((data) => {//data -> controller에서 리턴되는 데이터!
-        console.log(data);
-        let resultDiv = document.querySelector('.result');
-        for (const data1 of data) {
-            console.log(data1.memberId);
-            var resultEmail = document.createElement("input");
-                resultEmail.className = "resultEmail";
-                resultEmail.value = data1.memberId;
-                resultDiv.appendChild(resultEmail);
-        }
-        resultDiv.style = "display : block";
-    })
-    //fetch 통신 실패 시 실행 영역
-    .catch(err=>{
-        alert('fetch error!\nthen 구문에서 오류가 발생했습니다.\n콘솔창을 확인하세요!');
-        console.log(err);
-    });
-=======
         body: new URLSearchParams({
             "member_mail1": document.querySelector('#memberEmail').value + document.querySelector('#email_host').value
         })
@@ -267,32 +172,15 @@ function findId() {
             alert('fetch error!\nthen 구문에서 오류가 발생했습니다.\n콘솔창을 확인하세요!');
             console.log(err);
         });
->>>>>>> 83a6a44fec44cb4a50def74bcbc8a7d776db12c9
 }
 
 
 function checkCode() {
-<<<<<<< HEAD
-    if (confirmCode == null) {
-        document.querySelector('.alertbox').innerHTML="유효하지 않은 코드 입니다."
-        document.querySelector('.alertbox').style.color='red';
-    } else {
-        let inputCode = document.querySelector('#verify_code').value
-        if (inputCode == confirmCode) {
-            alert("인증되었습니다! 새로운 비밀번호를 설정해주세요!")
-            const additionalInputDiv = document.getElementById("additionalInput");
-            additionalInputDiv.style.display = "none";
-            const changePassword = document.getElementById("changePassword")
-            changePassword.style.display ="block";
-            document.querySelector('#verifyButton').style = "display : none";
-        } else {
-            document.querySelector('.alertbox').innerHTML="인증번호를 정확하게 입력해주세요"
-            document.querySelector('.alertbox').style.color='red';
-=======
     console.log(confirmCode);
     if (confirmCode == null) {
         document.querySelector('.alertbox').innerHTML = "유효하지 않은 코드 입니다."
         document.querySelector('.alertbox').style.color = 'red';
+        document.querySelector('.bi.bi-key').setAttribute("fill", "#ff3f3f");
     } else {
         let inputCode = document.querySelector('#verify_code').value
         if (inputCode == confirmCode) {
@@ -304,41 +192,13 @@ function checkCode() {
         } else {
             document.querySelector('.alertbox').innerHTML = "인증번호를 정확하게 입력해주세요"
             document.querySelector('.alertbox').style.color = 'red';
->>>>>>> 83a6a44fec44cb4a50def74bcbc8a7d776db12c9
+            document.querySelector('.bi.bi-key').setAttribute("fill", "#ff3f3f");
         }
     }
 
 }
 let confirmCode = null
 
-<<<<<<< HEAD
-function changePassword() {
-    const memberId = document.querySelector('#memberId').value;
-    const memberPw = document.querySelector('#memberPw').value;
-    const confirmPassword = document.querySelector('#memberPwCheck').value;
-
-    if (memberId === '') {
-        inputInvalidate('.err-div', '아이디를 입력해주세요!');
-        return;
-    } else if (memberPw === '') {
-        inputInvalidate('.err-div', '새 비밀번호를 입력해주세요!');
-        return;
-    } else if (memberPwCheck === '') {
-        inputInvalidate('.err-div', '비밀번호를 확인해주세요!');
-        return;
-    } else if (memberPw !== confirmPassword) {
-        inputInvalidate('.err-div', '비밀번호가 일치하지 않습니다!');
-        return;
-    }
-
-    document.getElementById('changePasswordForm').submit();
-    alert('비밀번호가 변경되었습니다.\n다시 로그인 해주세요');
-}
-
-
-// Validate 실패시 메세지 설정
-function inputInvalidate(tagId, message){
-=======
 function changePw() {
     let pw = document.querySelector('#memberPw').value;
     let pwCheck = document.querySelector('#memberPwCheck').value;
@@ -363,16 +223,18 @@ function changePw() {
 
 // Validate 실패시 메세지 설정
 function inputInvalidate(tagId, message) {
->>>>>>> 83a6a44fec44cb4a50def74bcbc8a7d776db12c9
     document.querySelector(tagId).style.display = 'block';
     document.querySelector(tagId).textContent = message;
 }
 
-<<<<<<< HEAD
-=======
 function goPw(){
     let memberId = document.querySelector('#memberId').value;
     var popup = window.open(`/member/changePwForm?memberId=${memberId}`, '비밀번호 변경', 'width=700px,height=800px,scrollbars=yes');
 }
->>>>>>> 83a6a44fec44cb4a50def74bcbc8a7d776db12c9
 
+$('#memberId').click(function(){
+    document.querySelector('.bi.bi-person-circle').setAttribute("fill", "#03c75a");
+})
+$('#memberEmail').click(function(){
+    document.querySelector('.bi.bi-envelope-at-fill').setAttribute("fill", "#03c75a");
+})
