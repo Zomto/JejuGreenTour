3f50e55e9746860f30cfc2a5395db2a56ab3beb0
SJY5261 <sjy5261@gmail.com>
2023-11-08 17:01:01 +0900

    1108 끝

---
+++ src/main/java/com/jejugreentour/jgt/csCenter/vo/ResponseVO.java
@@ -0,0 +1,11 @@
+package com.jejugreentour.jgt.csCenter.vo;
+
+import lombok.Data;
+
+@Data
+public class ResponseVO {
+    private String resCode;
+    private String inqCode;
+    private String inqResponse;
+    private String resDate;
+}
